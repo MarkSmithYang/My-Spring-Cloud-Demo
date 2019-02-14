af29fa26fb234f4c3a13bcbcb692a303799ab8cc
yangbiao <itbiaoyang@163.com>
2019-02-14 10:11:38 +0800

    a

---
+++ eureka-client-query/src/test/java/com/yb/eurekaclientquery/EurekaClientQueryApplicationTests.java
@@ -0,0 +1,17 @@
+package com.yb.eurekaclientquery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaClientQueryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
---
+++ eureka-server/src/main/java/com/yb/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.yb.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//@EnableEurekaServer指明这是一个eureka的服务注册中心服务
+//默认情况下这个服务也是个eureka-client(客户端),必须制定一个eureka-server,
+//所以需要在配置文件配置声明这个服务是一个server而不是client,实例在server注册后会发送心跳,心跳停止一定时间将被剔出除服务
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
+
